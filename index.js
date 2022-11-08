cf35f63fd84d3acce137354f5932658d90933a8a
Luca <tataeqs@gmail.com>
2022-11-08 18:20:37 -0300

    subido de calculadora

---
+++ index.js
@@ -0,0 +1,24 @@
+function suma(numero1, numero2){
+    numero1= parseInt(prompt("Ingrese su primer numero (1/2): "))
+    numero2= parseInt(prompt("Ingrese su segundo numero (2/2): "))
+    var resultadoSuma= numero1 + numero2;
+    alert("el resultado es "+ resultadoSuma)
+}
+function resta(numero1, numero2){
+    numero1= parseInt(prompt("Ingrese su primer numero (1/2): "))
+    numero2= parseInt(prompt("Ingrese su segundo numero (2/2): "))
+    var resultadoResta= numero1 - numero2;
+    alert ("el resultado es "+ resultadoResta)
+}
+function dividir(numero1, numero2){
+    numero1= parseInt(prompt("Ingrese su primer numero (1/2): "))
+    numero2= parseInt(prompt("Ingrese su segundo numero (2/2): "))
+    var resultadoDividir= numero1 / numero2;
+    alert ("el resultado es "+ resultadoDividir)
+}
+function multiplicar(numero1, numero2){
+    numero1= parseInt(prompt("Ingrese su primer numero (1/2): "))
+    numero2= parseInt(prompt("Ingrese su segundo numero (2/2): "))
+    var resultadoMultiplicar= numero1 * numero2;
+    alert ("el resultado es "+ resultadoMultiplicar)
+}
